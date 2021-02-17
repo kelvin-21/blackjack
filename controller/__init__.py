d79948bfa394c9d4c7cbe1ad1426480370a144dc
kelvin-21 <53601030+kelvin-21@users.noreply.github.com>
2021-02-17 21:33:15 +0800

    update module init

--- controller/__init__.py
+++ controller/__init__.py
@@ -1,2 +1,3 @@
-from .argument_handler import ArgumentHandler
+from .game_status import GameStatus
+from .argument_handler import ArgumentHandler, ArgumentTask
 from .game_controller import GameController
